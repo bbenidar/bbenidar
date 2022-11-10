2474ccafa9073c39737c0b370d061ea51f22dd61
Brahim Benidar <bbenidar@e1r7p14.1337.ma>
2022-11-10 18:30:36 +0100

    bbenidar

---
+++ printf/ft_print_hex.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_hex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbenidar <bbenidar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 17:44:27 by bbenidar          #+#    #+#             */
+/*   Updated: 2022/11/10 15:20:47 by bbenidar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+int	ft_hex_len(unsigned	int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
+
+void	ft_put_hex(unsigned int num, const char format)
+{
+	if (num >= 16)
+	{
+		ft_put_hex(num / 16, format);
+		ft_put_hex(num % 16, format);
+	}
+	else
+	{
+		if (num <= 9)
+			ft_putchar((num + '0'));
+		else
+		{
+			if (format == 'x')
+				ft_putchar((num - 10 + 'a'));
+			if (format == 'X')
+				ft_putchar((num - 10 + 'A'));
+		}
+	}
+}
+
+int	ft_print_hex(unsigned int num, const char format)
+{
+	if (num == 0)
+		return (write(1, "0", 1));
+	else
+		ft_put_hex(num, format);
+	return (ft_hex_len(num));
+}
